dfe7518518f99d2a4a47b06eb13462fdde2d954c
Sean Behan <codebam@riseup.net>
2023-08-16 16:23:00 -0400

    remove unused

--- packages/main/src/types.ts
+++ packages/main/src/types.ts
@@ -2,7 +2,6 @@ import BotApi from "./bot_api";
 import Handler from "./handler";
 import TelegramApi from "./telegram_api";
 import Webhook from "./webhook";
-import TelegramWebhook from "./telegram_webhook";
 export { Webhook };
 
 export type Command = (
