e208c29f146c8c997b59c9b7ba142db873b27e45
Sean Behan <codebam@riseup.net>
2023-11-15 19:32:19 -0500

    fix /start

--- packages/main/src/telegram_bot.ts
+++ packages/main/src/telegram_bot.ts
@@ -261,72 +261,72 @@ export default class TelegramBot extends TelegramApi {
 							new TelegramInlineQueryResultArticle("https://duckduckgo.com"),
 					  ])
 					: update.inline_query
-					? fetch(
-							addSearchParams(new URL("https://api.duckduckgo.com"), {
-								q: query,
-								format: "json",
-								t: "telegram_bot",
-								no_redirect: "1",
-							}).href
-					  ).then((response) =>
-							response
-								.json()
-								.then((results) => results as DDGQueryResponse)
-								.then((ddg_response) =>
-									((
-										instant_answer_url,
-										thumb_url,
-										default_thumb_url = "https://duckduckgo.com/assets/icons/meta/DDG-icon_256x256.png"
-									) =>
-										this.answerInlineQuery(
-											update.inline_query?.id ?? 0,
-											instant_answer_url !== ""
-												? [
-														new TelegramInlineQueryResultArticle(
-															`${instant_answer_url}\n\n<a href="${
-																addSearchParams(new URL(duckduckgo_url), {
-																	q: args
-																		.slice(1)
-																		.join(" ")
-																		.replace(/^!\w* /, ""),
-																}).href
-															}">Results From DuckDuckGo</a>`,
-															instant_answer_url,
-															"HTML",
-															thumb_url
-														),
-														new TelegramInlineQueryResultArticle(
-															duckduckgo_url,
-															duckduckgo_url,
-															"",
-															default_thumb_url
-														),
-												  ]
-												: [
-														new TelegramInlineQueryResultArticle(
-															duckduckgo_url,
-															duckduckgo_url,
-															"",
-															default_thumb_url
-														),
-												  ],
-											3600 // 1 hour
-										))(
-										ddg_response.Redirect ?? ddg_response.AbstractURL,
-										ddg_response.Redirect === ""
-											? `https://duckduckgo.com${
-													ddg_response.Image !== "" && ddg_response.Image
-														? ddg_response.Image
-														: ddg_response.RelatedTopics.length !== 0 &&
-														  ddg_response.RelatedTopics[0].Icon.URL !== ""
-														? ddg_response.RelatedTopics[0].Icon.URL
-														: "/i/f96d4798.png"
-											  }`
-											: ""
+					  ? fetch(
+								addSearchParams(new URL("https://api.duckduckgo.com"), {
+									q: query,
+									format: "json",
+									t: "telegram_bot",
+									no_redirect: "1",
+								}).href
+					    ).then((response) =>
+								response
+									.json()
+									.then((results) => results as DDGQueryResponse)
+									.then((ddg_response) =>
+										((
+											instant_answer_url,
+											thumb_url,
+											default_thumb_url = "https://duckduckgo.com/assets/icons/meta/DDG-icon_256x256.png"
+										) =>
+											this.answerInlineQuery(
+												update.inline_query?.id ?? 0,
+												instant_answer_url !== ""
+													? [
+															new TelegramInlineQueryResultArticle(
+																`${instant_answer_url}\n\n<a href="${
+																	addSearchParams(new URL(duckduckgo_url), {
+																		q: args
+																			.slice(1)
+																			.join(" ")
+																			.replace(/^!\w* /, ""),
+																	}).href
+																}">Results From DuckDuckGo</a>`,
+																instant_answer_url,
+																"HTML",
+																thumb_url
+															),
+															new TelegramInlineQueryResultArticle(
+																duckduckgo_url,
+																duckduckgo_url,
+																"",
+																default_thumb_url
+															),
+													  ]
+													: [
+															new TelegramInlineQueryResultArticle(
+																duckduckgo_url,
+																duckduckgo_url,
+																"",
+																default_thumb_url
+															),
+													  ],
+												3600 // 1 hour
+											))(
+											ddg_response.Redirect ?? ddg_response.AbstractURL,
+											ddg_response.Redirect === ""
+												? `https://duckduckgo.com${
+														ddg_response.Image !== "" && ddg_response.Image
+															? ddg_response.Image
+															: ddg_response.RelatedTopics.length !== 0 &&
+															    ddg_response.RelatedTopics[0].Icon.URL !== ""
+															  ? ddg_response.RelatedTopics[0].Icon.URL
+															  : "/i/f96d4798.png"
+												  }`
+												: ""
+										)
 									)
-								)
-					  )
-					: this.sendMessage(update.message?.chat.id ?? 0, duckduckgo_url))(
+					    )
+					  : this.sendMessage(update.message?.chat.id ?? 0, duckduckgo_url))(
 				query === ""
 					? "https://duckduckgo.com"
 					: (() => {
@@ -486,7 +486,7 @@ export default class TelegramBot extends TelegramApi {
 	commandList = async (update: TelegramUpdate): Promise<Response> =>
 		this.sendMessage(
 			update.message?.chat.id ?? 0,
-			`<pre>${JSON.stringify(Object.keys(this.commands))}</pre>`,
+			`${Object.keys(this.commands).join("\n")}`,
 			"HTML"
 		);
 
